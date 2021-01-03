1dc089ef284b02f53de5a6a425b09158146dceab
manabapp <manabapp>
2021-01-03 16:58:18 +0900

    Initial Commit

---
+++ Apps/AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppsApp.swift
+//  Apps
+//
+//  Created by Hirose Manabu on 2021/01/03.
+//
+
+import SwiftUI
+
+@main
+struct AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
